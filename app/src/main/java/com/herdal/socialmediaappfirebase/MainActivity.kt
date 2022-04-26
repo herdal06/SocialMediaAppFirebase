0ce18e16b4fe8c3dbbdb31b668fbab29c635d285
herdal06 <h.erdal721@gmail.com>
2022-04-26 05:58:06 +0300

    Setup bottom navigation

--- app/src/main/java/com/herdal/socialmediaappfirebase/MainActivity.kt
+++ app/src/main/java/com/herdal/socialmediaappfirebase/MainActivity.kt
@@ -2,10 +2,22 @@ package com.herdal.socialmediaappfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.herdal.socialmediaappfirebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navHostFragment.navController)
+
     }
 }
