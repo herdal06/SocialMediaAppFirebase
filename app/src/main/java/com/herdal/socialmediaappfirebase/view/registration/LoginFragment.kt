33639ded85591c6c75645c6d64afd5c924a05f66
herdal06 <h.erdal721@gmail.com>
2022-04-28 05:11:52 +0300

    Enable viewbinding and set click listener on textviews

--- app/src/main/java/com/herdal/socialmediaappfirebase/view/registration/LoginFragment.kt
+++ app/src/main/java/com/herdal/socialmediaappfirebase/view/registration/LoginFragment.kt
@@ -5,12 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.herdal.socialmediaappfirebase.R
+import com.herdal.socialmediaappfirebase.databinding.FragmentLoginBinding
+import com.herdal.socialmediaappfirebase.databinding.FragmentSignUpBinding
 
 class LoginFragment : Fragment() {
 
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.textViewGoToSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
 
         return view
     }
--- app/src/main/java/com/herdal/socialmediaappfirebase/view/registration/SignUpFragment.kt
+++ app/src/main/java/com/herdal/socialmediaappfirebase/view/registration/SignUpFragment.kt
@@ -5,13 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.herdal.socialmediaappfirebase.R
+import com.herdal.socialmediaappfirebase.databinding.FragmentSignUpBinding
 
 class SignUpFragment : Fragment() {
 
+    private var _binding: FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.textViewGoToLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_signUpFragment_to_loginFragment)
+        }
 
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
