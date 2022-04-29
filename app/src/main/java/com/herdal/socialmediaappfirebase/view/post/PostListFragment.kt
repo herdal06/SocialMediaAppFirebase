58dffd07796597b52f1ca63d661d6bbcc86c0a84
herdal06 <h.erdal721@gmail.com>
2022-04-29 07:33:53 +0300

    Log out with firebase

--- app/src/main/java/com/herdal/socialmediaappfirebase/view/post/PostListFragment.kt
+++ app/src/main/java/com/herdal/socialmediaappfirebase/view/post/PostListFragment.kt
@@ -1,17 +1,53 @@
 package com.herdal.socialmediaappfirebase.view.post
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.herdal.socialmediaappfirebase.R
+import com.herdal.socialmediaappfirebase.databinding.FragmentPostListBinding
 
 class PostListFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_post_list, container, false)
+    private var _binding: FragmentPostListBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentPostListBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        setHasOptionsMenu(true)
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        auth = Firebase.auth
+
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logOut) {
+            auth.signOut()
+            findNavController().navigate(R.id.action_postListFragment_to_loginFragment)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
